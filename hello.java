2b54556c0a03cba6656d2189cac4b7ac98e43606
nunnunannaa <hhj9523@gmail.com>
2018-10-28 12:38:32 +0900

    Third Commit

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String[] args){
     System.out.println("Hello, world from GiT!");
     System.out.println("Changes From the First commit");
+    System.out.println("Changes From the Second Commit");
   }
 }
